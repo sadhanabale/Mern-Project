50844f74ef5cd5e9473def67c49bddfa63644ce2
sadhanabale <33593773+sadhanabale@users.noreply.github.com>
2024-12-31 11:23:19 +0530

    app.js changes

--- Ecommerce-Backend-Project/src/app.js
+++ Ecommerce-Backend-Project/src/app.js
@@ -1,5 +1,5 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb'); // check this if needed
 const mongoose = require('mongoose');
 const dbURL = "mongodb+srv://project_user:EnTNtbJgNSXvhm4Y@cluster0.jdq8n60.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true";
 

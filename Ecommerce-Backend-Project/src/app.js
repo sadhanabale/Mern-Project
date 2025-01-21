f4451acb8587b119141d6b7b2ceca7dfbd17de8c
sadhanabale <sadhana.v1989@gmail.com>
2025-01-21 11:16:49 +0530

    products model controller added

--- Ecommerce-Backend-Project/src/app.js
+++ Ecommerce-Backend-Project/src/app.js
@@ -2,6 +2,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb'); // check this if needed
 const mongoose = require('mongoose');
 const express = require('express');
+
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require ('./routes/productRoutes');
+
 // const dotenv = require('dotenv');
 
 // dotenv.config();
@@ -18,69 +22,10 @@ mongoose.connect(dbURL).then((connection)=>{
      console.log('db is connected');
 });
 
-const userSchemaObject = {
-    name:{
-        type:String,
-        required: true
-    },
-    email:{
-        type:String,
-        required: true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required: true,
-        minlength: 8,
-    },
-    confirmPassword:{
-        type:String,
-        required: true,
-        minlength: 8,
-        validate: function(){
-            return this.password === this.confirmPassword
-        }
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-    },
-
-}
-
-const userSchema = new mongoose.Schema(userSchemaObject);
-
-const UserModel= mongoose.model("UserModel",userSchema);
-
-console.log(UserModel);
-
 app.use(express.json());
 
-const createUser = async(req,res) =>{
-    try{
-        console.log(req.body);
-        let user = UserModel.create(req.body);
-
-        if(!user){
-            res.status(400).json({
-                status:"Failure",
-                data:user
-        });
-        }
-
-        res.status(200).json({
-            status:"Success",
-            data:user
-    });
-       
-    }catch(err){
-        res.status(500).json({
-            message:"Internal server error"
-        });
-    }
-}
-
-app.use('/api/users',createUser);
+app.use('/api/users',userRoutes);
+app.use('/api/products',productRoutes);
 
 const port = 3000;
 
